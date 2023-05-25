a2ab91d59ab965864cf7bb58915ca58bd3480262
zixx-bit <65281903+zixx-bit@users.noreply.github.com>
2023-05-25 19:09:26 +0300

    Bluetooth Print App

--- app/src/main/java/com/example/bluetoothprint/MainActivity.kt
+++ app/src/main/java/com/example/bluetoothprint/MainActivity.kt
@@ -2,10 +2,44 @@ package com.example.bluetoothprint
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.mazenrashed.printooth.utilities.Printing
+import com.mazenrashed.printooth.utilities.PrintingCallback
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), PrintingCallback {
+
+    internal var printing:Printing ?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initview()
+
+    }
+
+    private fun initview() {
+        if (printing != null)
+            printing!!.printingCallback = this
+
+
+    }
+
+    override fun connectingWithPrinter() {
+        TODO("Not yet implemented")
+    }
+
+    override fun connectionFailed(error: String) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onError(error: String) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onMessage(message: String) {
+        TODO("Not yet implemented")
+    }
+
+    override fun printingOrderSentSuccessfully() {
+        TODO("Not yet implemented")
     }
 }
