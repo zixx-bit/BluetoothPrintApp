8d04b7415a0d0fe4443e9da469c6500306cf59ea
zixx-bit <65281903+zixx-bit@users.noreply.github.com>
2023-05-25 20:01:07 +0300

    Bluetooth Print App

--- app/src/main/java/com/example/bluetoothprint/MainActivity.kt
+++ app/src/main/java/com/example/bluetoothprint/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.example.bluetoothprint
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.mazenrashed.printooth.Printooth
+import com.mazenrashed.printooth.ui.ScanningActivity
 import com.mazenrashed.printooth.utilities.Printing
 import com.mazenrashed.printooth.utilities.PrintingCallback
 
@@ -20,7 +24,23 @@ class MainActivity : AppCompatActivity(), PrintingCallback {
         if (printing != null)
             printing!!.printingCallback = this
 
+            btnPairUnpair.setOnClickListener{
+                if (Printooth.hasPairedPrinter())
+                    Printooth.removeCurrentPrinter()
+                else{
+                    startActivityForResult(Intent(this@MainActivity, ScanningActivity::class.java),
+                        ScanningActivity.SCANNING_FOR_PRINTER)
+                    changePairAndUnpair()
+                }
+            }
 
+    }
+
+    private fun changePairAndUnpair() {
+        if (Printooth.hasPairedPrinter())
+            btnPairUnPair.text = "Unpair ${Printooth.getPairedPrinter()!!.name}"
+        else
+            btnPairUnpair.text = "Pair with Printer"
     }
 
     override fun connectingWithPrinter() {
