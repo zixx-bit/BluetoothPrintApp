82cb5dd21e4af652cf5106cc8ad93041e2a9f316
zixx-bit <65281903+zixx-bit@users.noreply.github.com>
2023-05-25 18:47:46 +0300

    Bluetooth Print App

--- app/src/main/java/com/example/bluetoothprint/ApplicationClass.kt
+++ app/src/main/java/com/example/bluetoothprint/ApplicationClass.kt
@@ -1,6 +1,11 @@
 package com.example.bluetoothprint
 
 import android.app.Application
+import com.mazenrashed.printooth.Printooth
 
-class ApplicationClass: Application{
+class ApplicationClass: Application(){
+    override fun onCreate() {
+        super.onCreate()
+        Printooth.init(this)
+    }
 }
